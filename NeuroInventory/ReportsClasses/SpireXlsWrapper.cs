3b347fb407b67bf0248b40ccfe152ffbdad0e192
neuro256 <neuro256@mail.ru>
2017-06-12 23:03:16 +0900

    Добавлена библиотека для работы с xls - Spire.Xls 7.9.1 + Создан класс SpireXlsWrapper.cs + Добавлен шаблон templateDebit.xls

---
+++ NeuroInventory/ReportsClasses/SpireXlsWrapper.cs
@@ -0,0 +1,16 @@
+﻿using Spire.Xls;
+
+namespace NeuroInventory
+{
+    public class SpireXlsWrapper
+    {
+        private string m_TemplateSourcePath = @"templateDebit.xls";
+
+        public bool CreateReport()
+        {
+            Workbook book = new Workbook();
+            
+            return true;
+        }
+    }
+}

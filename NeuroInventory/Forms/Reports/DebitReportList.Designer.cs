84f0f9eaaabc6db328003c53dfe04527daf227c3
neuro256 <neuro256@mail.ru>
2017-06-14 00:20:08 +0900

    Добавлено окно DebitReportList

---
+++ NeuroInventory/Forms/Reports/DebitReportList.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace NeuroInventory
+{
+    partial class DebitReportList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.lvDebitReportList = new System.Windows.Forms.ListView();
+            this.columnHeaderId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDoc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panelDebitReportListBottom = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.contextMenuStripDebitReportList = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.panelDebitReportListBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.lvDebitReportList, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.panelDebitReportListBottom, 0, 1);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(782, 560);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // lvDebitReportList
+            // 
+            this.lvDebitReportList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvDebitReportList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderId,
+            this.columnHeaderNumber,
+            this.columnHeaderDate,
+            this.columnHeaderDoc});
+            this.lvDebitReportList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvDebitReportList.FullRowSelect = true;
+            this.lvDebitReportList.GridLines = true;
+            this.lvDebitReportList.Location = new System.Drawing.Point(3, 3);
+            this.lvDebitReportList.MultiSelect = false;
+            this.lvDebitReportList.Name = "lvDebitReportList";
+            this.lvDebitReportList.Size = new System.Drawing.Size(776, 504);
+            this.lvDebitReportList.TabIndex = 0;
+            this.lvDebitReportList.UseCompatibleStateImageBehavior = false;
+            this.lvDebitReportList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderId
+            // 
+            this.columnHeaderId.Text = "id";
+            // 
+            // columnHeaderNumber
+            // 
+            this.columnHeaderNumber.Text = "#";
+            // 
+            // columnHeaderDate
+            // 
+            this.columnHeaderDate.Text = "Дата";
+            this.columnHeaderDate.Width = 100;
+            // 
+            // columnHeaderDoc
+            // 
+            this.columnHeaderDoc.Text = "Документ";
+            this.columnHeaderDoc.Width = 250;
+            // 
+            // panelDebitReportListBottom
+            // 
+            this.panelDebitReportListBottom.Controls.Add(this.btnDelete);
+            this.panelDebitReportListBottom.Controls.Add(this.btnAdd);
+            this.panelDebitReportListBottom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDebitReportListBottom.Location = new System.Drawing.Point(3, 513);
+            this.panelDebitReportListBottom.Name = "panelDebitReportListBottom";
+            this.panelDebitReportListBottom.Size = new System.Drawing.Size(776, 44);
+            this.panelDebitReportListBottom.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(113, 6);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 29);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(9, 6);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(98, 29);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // contextMenuStripDebitReportList
+            // 
+            this.contextMenuStripDebitReportList.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStripDebitReportList.Name = "contextMenuStripDebitReportList";
+            this.contextMenuStripDebitReportList.Size = new System.Drawing.Size(67, 4);
+            // 
+            // DebitReportList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 560);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "DebitReportList";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Список требований";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.panelDebitReportListBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.ListView lvDebitReportList;
+        private System.Windows.Forms.ColumnHeader columnHeaderId;
+        private System.Windows.Forms.ColumnHeader columnHeaderNumber;
+        private System.Windows.Forms.ColumnHeader columnHeaderDate;
+        private System.Windows.Forms.ColumnHeader columnHeaderDoc;
+        private System.Windows.Forms.Panel panelDebitReportListBottom;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripDebitReportList;
+    }
+}
---
+++ NeuroInventory/Forms/Reports/DebitReportList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NeuroInventory
+{
+    public partial class DebitReportList : InventoryView
+    {
+        public DebitReportList()
+        {
+            InitializeComponent();
+            SQLiteManager.GetInstance().DebitReport().SetCommandDataSet();
+            InitForm();
+            InitListView();
+            InitContextMenuStrip();
+            ShowTable();
+            m_ListviewSelectedIndex = 0;
+        }
+
+        protected override void InitForm()
+        {
+            this.Size = new Size(800, 600);
+            this.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ControlBox = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+
+            this.Name = "DebitReportList";
+            this.Text = "Список списаний";
+        }
+
+        public override void InitListView()
+        {
+            base.InitListView();
+
+            lvDebitReportList.Columns.Clear();
+            lvDebitReportList.Columns.Add(new ColHeader("ID", 50, HorizontalAlignment.Left, true));
+            lvDebitReportList.Columns.Add(new ColHeader("№", 50, HorizontalAlignment.Left, true));
+            lvDebitReportList.Columns.Add(new ColHeader("Дата", 100, HorizontalAlignment.Left, true));
+            lvDebitReportList.Columns.Add(new ColHeader("Документ", 250, HorizontalAlignment.Left, true));
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddRecord();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            RemoveRecord();
+        }
+
+        public override void AddRecord()
+        {
+            DebitReport report = new DebitReport();
+            report.StartPosition = FormStartPosition.CenterParent;
+            if(report.ShowDialog() == DialogResult.OK)
+            {
+                SQLiteManager.GetInstance().DebitReport().SetCommandDataSet();
+                ShowTable();
+            }
+        }
+
+        public override void RemoveRecord()
+        {
+            if(m_Listview.SelectedItems.Count > 0)
+            {
+                SQLiteManager.GetInstance().DebitReport().Remove(m_ListviewSelectedIndex);
+                m_Listview.SelectedItems.Clear();
+                SQLiteManager.GetInstance().DebitReport().SetCommandDataSet();
+                ShowTable();
+            }
+            else
+            {
+                MessageBox.Show(Definitions.REMOVE_WARNING_STRING);
+            }
+        }
+
+        protected override ListView GetListView()
+        {
+            return lvDebitReportList;
+        }
+
+        protected override ContextMenuStrip GetContextMenuStrip()
+        {
+            return contextMenuStripDebitReportList;
+        }
+
+        public override DataSet ReturnDataSet()
+        {
+            return SQLiteManager.GetInstance().DebitReport().ReturnDataSet();
+        }
+
+        public override void ListViewItemMouseUp(object sender, MouseEventArgs e)
+        {
+            base.ListViewItemMouseUp(sender, e);
+
+            if(m_Listview.GetItemAt(e.X, e.Y)?.SubItems["document"]?.Bounds.Contains(e.X, e.Y) ?? false)
+            {
+                string l_FileName = m_Listview.GetItemAt(e.X, e.Y)?.SubItems["document"].Tag.ToString();
+                NeuroFile.GetInstance().OpenFileInExplorer(l_FileName);
+                m_Listview.SelectedItems.Clear();
+            }
+        }
+
+        public override void ShowTable()
+        {
+            if (!SQLiteManager.GetInstance().TestConnection())
+                return;
+            DataSet dataSet = ReturnDataSet();
+            try
+            {
+                //Заполняем список
+                m_Listview.BeginUpdate();
+                m_Listview.Items.Clear();
+                for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+                {
+                    m_Listview.Items.Add(dataSet.Tables[0].Rows[i]["id"].ToString());
+                    m_Listview.Items[i].SubItems.Add((i + 1).ToString());
+                    for (int j = 1; j < dataSet.Tables[0].Columns.Count; j++)
+                    {
+                        ListViewItem.ListViewSubItem subitem = new ListViewItem.ListViewSubItem();
+                        subitem.Text = dataSet.Tables[0].Rows[i][j].ToString();
+                        subitem.Name = dataSet.Tables[0].Columns[j].ToString();
+                        if(subitem.Name == "document")
+                        {
+                            subitem.BackColor = Color.LightBlue;
+                            subitem.Tag = subitem.Text;
+                            subitem.Text = Path.GetFileName(subitem.Text);
+                        }
+                        m_Listview.Items[i].SubItems.Add(subitem);
+                    }
+                    m_Listview.Items[i].UseItemStyleForSubItems = false;
+                }
+                m_Listview.EndUpdate();
+            }
+            catch (SQLiteException se)
+            {
+                MessageBox.Show(se.Message, "Ошибка подключения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException se)
+            {
+                MessageBox.Show("Error!:", se.Message);
+            }
+            finally
+            {
+                dataSet.Dispose();
+            }
+        }
+    }
+}

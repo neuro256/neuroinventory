ec6e4f83adedef77420d31faeebfee12ed3d0544
neuro256 <neuro256@mail.ru>
2017-06-17 21:00:44 +0900

    Обновление данных инсталлятора

--- NeuroInventory/Properties/AssemblyInfo.cs
+++ NeuroInventory/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.3")]
+[assembly: AssemblyVersion("0.3.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

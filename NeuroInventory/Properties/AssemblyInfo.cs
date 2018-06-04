d37e13e8190cdba26eea11d0d2b2f19c05139ab5
Alexey Stepanov <neuro256@mail.ru>
2018-06-05 00:00:36 +0900

    Версия повышена до 0.6.4

--- NeuroInventory/Properties/AssemblyInfo.cs
+++ NeuroInventory/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.3")]
+[assembly: AssemblyVersion("0.6.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
